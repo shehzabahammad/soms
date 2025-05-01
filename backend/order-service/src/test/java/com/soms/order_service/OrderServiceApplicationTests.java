b9bba8bde86186a7def5177d2ac29dc72c6aa18a
shehzab <>
2025-05-01 14:59:29 +0530

    user & order service

---
+++ backend/order-service/src/test/java/com/soms/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soms.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
